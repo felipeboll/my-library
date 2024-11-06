73315aa398e64828865e95de3897c35c8e8a6003
Felipe Boll Borges <felipe@MacBook-Air-de-Felipe.local>
2024-11-05 22:40:02 -0300

    Finalizei o script para iterar o array e dar o display dos cardes na tela. Além disso, criei o modal para quando clicar no botao New Book aparecer o formulário na tela.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const ulBookList = document.querySelector('.book-list');
+const newBookBtn = document.querySelector('.add-new-book');
+const newBookForm = document.getElementById('new-book-form');
+const closeNewBookForm = document.querySelector('.close-new-book-form');
+
 const myLibrary = [];
 
-function Book(title, author, numberOfPages, review, cover, readingStatus){
+function Book(title, author, numberOfPages, rate, cover, readingStatus){
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
-    this.review = review;
+    this.rate= rate;
     this.cover = cover;
     this.readingStatus = readingStatus;
 }
@@ -17,11 +22,75 @@ myLibrary.push(sapiens1);
 myLibrary.push(sapiens2);
 myLibrary.push(sapiens3);
 
-function displayBooks(book){
+myLibrary.forEach(Book => {
+    displayBooks(Book);
+});
+
+function displayBooks(Book){
 
     const li = document.createElement('li');
     li.classList.add('book-card');
 
+    const img = document.createElement('img');
+    img.classList.add('book-card-cover');
+    img.setAttribute('src', Book.cover);
+
+    const bookRate = document.createElement('p');
+    bookRate.classList.add('book-card-rate');
+    bookRate.innerHTML = `Nota ${Book.rate}`;
+
+    const bookTitle = document.createElement('h3');
+    bookTitle.classList.add('book-card-title');
+    bookTitle.textContent = Book.title;
+
+    const div = document.createElement('div');
+    div.classList.add('book-card_information');
+
+    const author = document.createElement('p');
+    author.textContent = Book.author;
+
+    const pages = document.createElement('p');
+    pages.textContent = Book.numberOfPages;
+
+
+    div.append(author);
+    div.append(pages);
+
+    const select = document.createElement('select');
+    select.classList.add('book-card-reading-status');
+
+    const optionReading = document.createElement('option');
+    optionReading.value = 'reading';
+    optionReading.textContent = 'Reading';
+
+    const optionRead = document.createElement('option');
+    optionRead.value = 'read';
+    optionRead.textContent = 'Read';
+
+    const optionPending = document.createElement('option');
+    optionPending.value = 'pending';
+    optionPending.textContent = 'Pending';
+
+    select.append(optionPending);
+    select.append(optionReading);
+    select.append(optionRead);
+    
+    li.append(img);
+    li.append(bookRate);
+    li.append(bookTitle);
+    li.append(div);
+    li.append(select);
+
+    select.value = Book.readingStatus;
+
+    ulBookList.append(li);
 
 }
 
+newBookBtn.addEventListener('click', ()=>{
+    newBookForm.showModal();
+});
+
+closeNewBookForm.addEventListener('click', ()=>{
+    newBookForm.close();
+});
